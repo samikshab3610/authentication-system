2fbc3fb7acfdedc2c74003e2c565464688afa623
Samiksha Bhore <samikshabhore7636@gmail.com>
2026-03-27 00:05:12 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -106,6 +106,6 @@ app.post("/api/login", async (req, res) => {
 
 // start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
